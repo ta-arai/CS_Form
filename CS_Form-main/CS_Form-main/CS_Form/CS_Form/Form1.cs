9c7af2e90dfa56be2fc44918c698e7d18ad0c377
Taarai <zeal24360197@fsgcl.jp>
2024-11-01 16:05:59 +0900

    test

---
+++ CS_Form-main/CS_Form-main/CS_Form/CS_Form/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+            {
+
+
+
+                TestButton testButton = new TestButton((i % 5)* 100, (i % 2)* 100, 100, 100,i);
+
+                Controls.Add(testButton);
+            }
+           // TestButton testButton2 = new TestButton(100,100,100,100);
+           //
+           // Controls.Add(testButton2);
+           //
+           // TestButton testButton3 = new TestButton(100, 0, 100, 100);
+           //
+           // Controls.Add(testButton3);
+           //
+           // TestButton testButton4 = new TestButton(100, 200, 100, 100);
+           //
+           // Controls.Add(testButton4);
+           //
+           // TestButton testButton5 = new TestButton(200, 0, 100, 100);
+           //
+           // Controls.Add(testButton5);
+           //
+           // TestButton testButton6 = new TestButton(200, 100, 100, 100);
+           //
+           // Controls.Add(testButton6);
+           //
+           // TestButton testButton7 = new TestButton(200, 200, 100, 100);
+           //
+           // Controls.Add(testButton7);
+           //
+           // TestButton testButton8 = new TestButton(300, 0, 100, 100);
+           //
+           // Controls.Add(testButton8);
+           //
+           // TestButton testButton9 = new TestButton(300, 100, 100, 100);
+           //
+           // Controls.Add(testButton9);
+           //
+           // TestButton testButton10 = new TestButton(300, 200, 100, 100);
+           //
+           // Controls.Add(testButton10);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CS_Form-main/CS_Form-main/CS_Form/CS_Form/TestButton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+
+namespace CS_Form
+{
+    class TestButton : Button
+    {
+
+        /// <summary>
+        /// コンストラクタ
+        
+        /// クラスを生成したときに呼び出される
+        /// </summary>
+        public TestButton(int x , int y, int width,int height,int id)
+        {
+            // ClickイベントにOnClick関数を登録
+            // ボタンをクリックした時に登録した関数を実行します。
+            Click += OnClick;
+
+            Text = id.ToString();
+
+            Location = new System.Drawing.Point(x, y);
+
+            Size = new System.Drawing.Size(width,height);
+        }
+
+        /// <summary>
+        /// ボタンをクリックした際の処理を記述する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="s"></param>
+        public void OnClick(object sender, EventArgs s)
+        {
+            MessageBox.Show(Text);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
